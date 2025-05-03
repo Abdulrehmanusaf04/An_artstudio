77b4afe0a555b4daabe44bed352c0dcd3c4fcca8
Abdulrehmanusaf04 <aryousaf04@gmail.com>
2025-05-04 03:29:36 +0500

    Update Gallery.js

--- components/Gallery.js
+++ components/Gallery.js
@@ -5,22 +5,22 @@ const Gallery = () => {
   const artPieces = [
     { id: 1, title: 'Sunset Dreams', imgUrl: 'url-to-image-1.jpg' },
     { id: 2, title: 'Ocean Whisper', imgUrl: 'url-to-image-2.jpg' },
+    // Add more artworks
   ];
 
   return (
-    <div className="gallery-page"> {/* Added new wrapper class */}
-      <section className="gallery">
-        <h2>My Artworks</h2>
-        <div className="gallery-grid">
-          {artPieces.map((art) => (
-            <div key={art.id} className="art-card">
-              <img src={art.imgUrl} alt={art.title} />
-              <h3>{art.title}</h3>
-            </div>
-          ))}
-        </div>
-      </section>
-    </div>
+    <section id="gallery" className="gallery">
+      <h2>My Artworks</h2>
+      <div className="gallery-grid">
+        {artPieces.map((art) => (
+          <div key={art.id} className="art-card">
+            <img src={art.imgUrl} alt={art.title} />
+            <h3>{art.title}</h3>
+          </div>
+          
+        ))}
+      </div>
+    </section>
   );
 };
 
