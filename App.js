b16fc2662ccbe318326d9357db947fd799f33ea3
Abdulrehmanusaf04 <aryousaf04@gmail.com>
2025-05-04 03:26:07 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,25 +1,26 @@
-// The navigation added code
+// 1. Import React and required components
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom'; // If using routing
 import Navbar from './components/Navbar';
-import Home from './components/Home'; // We'll create this new component
+import Hero from './components/Hero';
 import Gallery from './components/Gallery';
 import About from './components/About';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import './styles/main.css';
+import './styles/main.css'; // Global styles
 
+// 2. Main App component
 function App() {
   return (
+    // 3. Wrap with Router if using navigation
     <Router>
       <div className="app">
+        {/* 4. Component hierarchy */}
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <Hero />
+        <Gallery />
+        <About />
+        <Contact />
         <Footer />
       </div>
     </Router>
