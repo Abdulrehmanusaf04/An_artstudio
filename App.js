5d55bb19d6ab2d19b88889b1aac6df7568c336ee
Abdulrehmanusaf04 <aryousaf04@gmail.com>
2025-05-06 01:16:05 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,30 +1,30 @@
-// 1. Import React and required components
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'; // If using routing
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Gallery from './components/Gallery';
-import About from './components/About';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import './styles/main.css'; // Global styles
+import { BrowserRouter as Router } from "react-router-dom" // If using routing
+import Navbar from "./components/Navbar"
+import Home from "./components/Home"
+import Gallery from "./components/Gallery"
+import Services from "./components/Services"
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
+import "./styles/main.css" // Global styles
 
-// 2. Main App component
+// Main App component
 function App() {
   return (
-    // 3. Wrap with Router if using navigation
+    // Wrap with Router if using navigation
     <Router>
       <div className="app">
-        {/* 4. Component hierarchy */}
+        {/* Component hierarchy */}
         <Navbar />
-        <Hero />
+        <Home />
         <Gallery />
+        <Services />
         <About />
         <Contact />
         <Footer />
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
